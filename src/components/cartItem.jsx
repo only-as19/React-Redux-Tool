2e3f414fa9d15a6f4d72b5747232662769580cdc
Asad <hellohello51981@gmail.com>
2025-10-06 12:16:07 +0500

    feat: Added Content for cartItem

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,9 +1,19 @@
-
-const CartItem = () => {
+import { BiChevronDown, BiChevronUp } from "react-icons/bi"
+const CartItem = ({id, img, amount, price, title}) => {
   return (
-    <div>
-      CartItem
-    </div>
+    <article>
+        <img src={img} alt={title} />
+        <div>
+            <h2>{title}</h2>
+            <p>${price}</p>
+            <button>Remove</button>
+        </div>
+        <div>
+          <button><BiChevronUp /></button>
+          <span>{amount}</span>
+          <button><BiChevronDown /></button>
+        </div>
+    </article>
   )
 }
 
