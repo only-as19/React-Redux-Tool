68dd5d4d2ab27e31f8347a5137838f281a43f7cc
Asad <hellohello51981@gmail.com>
2025-10-06 10:39:35 +0500

    feat: Style the Navbar & and update css by adding colors

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,21 @@
 import { useSelector } from "react-redux";
-import { CartIcon } from "../data/icons";
+import { BiShoppingBag } from "react-icons/bi";
+
 const Navbar = () => {
   const { amount } = useSelector((state) => state.cart);
 
   return (
-    <div>
-      <h3>Redux ToolKit</h3>
+    <nav>
+        <div className=" relative p-5 bg-primary text-white flex items-center justify-between">
+      <h3 className="md:text-5xl text-3xl font-bold">Redux ToolKit</h3>
       <div>
-        <CartIcon />
-        <div>
+        <BiShoppingBag size={40} />
+        <div className="absolute top-5 right-3 bg-primary-light h-6 w-6 text-center rounded-full">
           <p>{amount}</p>
         </div>
       </div>
     </div>
+    </nav>
   );
 };
 
